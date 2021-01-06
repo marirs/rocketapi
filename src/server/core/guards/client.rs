02a37641b0d9681074692597b890ff4501f12e39
Sriram Govindan <marirs@gmail.com>
2021-01-06 10:57:22 +0530

    client information (ip & user_agent)

---
+++ src/server/core/guards/client.rs
@@ -0,0 +1,54 @@
+use std::net::IpAddr;
+use rocket::{
+    http::Status,
+    request::{self, FromRequest, Request},
+    Outcome, State,
+};
+
+#[derive(Debug)]
+pub struct ClientInfo {
+    pub ip: String,
+    pub user_agent: String,
+}
+
+impl ClientInfo {
+    pub fn new(ip: String, user_agent: String) -> Self {
+        Self {
+            ip,
+            user_agent,
+        }
+    }
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for ClientInfo {
+    type Error = ();
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        let ua = request.headers().get("user-agent").next().unwrap();
+        let browser_info = if ua.is_empty() {
+            "".to_string()
+        } else {
+            ua.to_string()
+        };
+
+        match request.real_ip() {
+            Some(ip) => {
+                let state = ClientInfo::new(ip.to_string(), browser_info);
+                Outcome::Success(state)
+            }
+            None => {
+                match request.headers().get("x-forwarded-for").next() {
+                    Some(forwarded_ip) => {
+                        let forwarded_ip = forwarded_ip.split(",").next().unwrap();
+                        let state = ClientInfo::new(forwarded_ip.to_string(), browser_info);
+                        Outcome::Success(state)
+                    }
+                    None => {
+                        let ip = request.client_ip().unwrap();
+                        let state = ClientInfo::new(ip.to_string(), browser_info);
+                        Outcome::Success(state)
+                    }
+                }
+            }
+        }
+    }
+}
--- src/server/core/guards/mod.rs
+++ src/server/core/guards/mod.rs
@@ -1,3 +1,8 @@
+/// db
 pub mod db;
+
 /// filters
 pub(crate) mod filters;
+
+/// client information (client ip_address and client user_agent)
+pub(crate) mod client;
